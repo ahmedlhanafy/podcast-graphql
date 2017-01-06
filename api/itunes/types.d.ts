615ac465361cc95f8fa58ef9ade274e0aa216788
Ahmed Magdy <ahmed.elhanafy95@gmail.com>
2017-01-06 04:52:46 +0200

    Add connectors file, add typings for it and install node-fetch

---
+++ api/itunes/types.d.ts
@@ -0,0 +1,79 @@
+interface ItunesApiResult{
+  wrapperType: string
+  kind: string
+  collectionId: number
+  trackId: number
+  artistName: string
+  collectionName: string
+  trackName: string
+  collectionCensoredName: string
+  trackCensoredName: string
+  collectionViewUrl:string
+  feedUrl: string
+  trackViewUrl: string
+  artworkUrl30: string
+  artworkUrl60: string
+  artworkUrl100: string
+  collectionPrice:number
+  trackPrice:number
+  trackRentalPrice:number
+  collectionHdPrice:number
+  trackHdPrice:number
+  trackHdRentalPrice:number
+  releaseDate: string
+  collectionExplicitness:string
+  trackExplicitness:string
+  trackCount:number
+  country: string
+  currency: string
+  primaryGenreName: string
+  contentAdvisoryRating: string
+  artworkUrl600: string
+  genreIds: Array<number>
+  genres: Array<string>
+}
+
+interface ItunesApiResponse{
+  resultCount: number
+  results: Array<ItunesApiResult>
+}
+
+interface SearchPodcastsResult extends ItunesApiResult{
+  id: number
+  name: string
+  itunesUrl: string
+}
+
+interface ParsePodcastApiResponse{
+  title: string
+  description: {
+    short: string
+    description: string
+  }
+  link: string
+  image: string
+  language: string
+  copyright: string
+  updated: string
+  categories: Array<string>
+  owner: {
+    name: string
+    email: string
+  }
+  episodes: Array<Episode>
+}
+
+interface Episode{
+  guid: string
+  title: string
+  description: string
+  image: string
+  published: string
+  duration: number
+  categories: Array<string>
+  enclosure: {
+    filesize: number
+    type: string
+    url: string
+  }
+}
--- api/utils/parsePodcastPromise.ts
+++ api/utils/parsePodcastPromise.ts
@@ -1,7 +1,7 @@
 import parsePodcast from 'node-podcast-parser';
 
 // `node-podcast-parser` uses callbacks so this is a wrapper that converts it to a promise
-const parsePodcastPromise = data => new Promise((resolve, reject) => {
+const parsePodcastPromise = data => new Promise<ParsePodcastApiResponse>((resolve, reject) => {
   parsePodcast(data, (err, d) => {
     if (err) {
       reject(err);
