159a158b4bc2964e172605352cdeab7e49f228a8
Abdelrahman Maged <abdulrahman.maged95@gmail.com>
2017-01-12 23:13:30 +0200

    Beautify GraphQL schema

--- api/graphql/schema.ts
+++ api/graphql/schema.ts
@@ -1,64 +1,65 @@
 const schema = `
 type Podcast {
-  artist: Artist,
-  artworkUrls: Artwork,
-  country: String!,
-  episodes: [Episode],
-  feedUrl: String!,
-  genreIds: [Int]!,
-  genres: [String]!,
-  id: ID!,
-  itunesUrl: String!,
-  name: String!,
-  palette: Palette,
-  primaryGenreName: String,
-  releaseDate: String,
-  trackCount: Int!,
+  artist: Artist
+  artworkUrls: Artwork
+  country: String!
+  episodes: [Episode]
+  feedUrl: String!
+  genreIds: [Int]!
+  genres: [String]!
+  id: ID!
+  itunesUrl: String!
+  name: String!
+  palette: Palette
+  primaryGenreName: String
+  releaseDate: String
+  trackCount: Int!
 }
 
-type Artist{
-  name: String,
-  itunesUrl: String,
-  id: ID!,
+type Artist {
+  name: String
+  itunesUrl: String
+  id: ID!
 }
 
-type Artwork{
-  xsmall: String!,
-  small: String!,
-  medium: String!,
-  large: String!,
+type Artwork {
+  xsmall: String!
+  small: String!
+  medium: String!
+  large: String!
 }
 
 type Episode {
-  title: String,
-  description: String,
-  guid: String,
-  published: String,
-  duration: Int,
+  title: String
+  description: String
+  guid: String
+  published: String
+  duration: Int
   enclosure: Enclosure
 }
 
-type Palette{
-  vibrantColor: Color,
-  darkVibrantColor: Color,
+type Palette {
+  vibrantColor: Color
+  darkVibrantColor: Color
 }
 
-type Color{
-  rgbColor: String,
-  population: Int,
+type Color {
+  rgbColor: String
+  population: Int
 }
 
 type Enclosure {
-  filesize: Int,
-  type: String,
+  filesize: Int
+  type: String
   url: String
 }
+
 type Query {
   podcasts(name: String, limit: Int, id: ID): [Podcast]
 }
 
 schema {
-  query: Query,
+  query: Query
 }
 `;
 
