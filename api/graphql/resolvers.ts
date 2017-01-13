6b8e9b7cce4c19fffbdc5cc17ea6522c19070bbe
Abdelrahman Maged <abdulrahman.maged95@gmail.com>
2017-01-13 15:24:19 +0200

    :package: Factor out GraphQL resolvers into functions

--- api/graphql/resolvers.ts
+++ api/graphql/resolvers.ts
@@ -1,54 +1,74 @@
 import { findAllPodcasts, findOnePodcast, searchEpisodes } from '../itunes/connectors';
 import { extractColors, formatColor } from '../utils';
 
+const resolvePodcasts = async ({ id, name, limit }) => {
+  let results;
+  if (id) {
+    results = await findOnePodcast({ id });
+  } else {
+    results = await findAllPodcasts({ name, limit });
+  }
+  return results.map(podcast => ({
+    ...podcast,
+    id: podcast.collectionId,
+    name: podcast.collectionName,
+    itunesUrl: podcast.collectionViewUrl,
+  }));
+};
+
+const resolveEpisodes = ({ feedUrl }, { limit }) => {
+  return searchEpisodes({ feedUrl, limit });
+};
+
+const resolveArtworkUrls = ({ artworkUrl30, artworkUrl60, artworkUrl100, artworkUrl600 }) => {
+  return {
+    xsmall: artworkUrl30,
+    small: artworkUrl60,
+    medium: artworkUrl100,
+    large: artworkUrl600,
+  };
+};
+
+const resolveArtist = ({ artistId, artistName, artistViewUrl }) => {
+  return {
+    id: artistId,
+    name: artistName,
+    itunesUrl: artistViewUrl,
+  };
+};
+
+const resolvePalette = async ({ artworkUrl60 }) => {
+  const colorPalette = await extractColors(artworkUrl60);
+  return {
+    vibrantColor: {
+      rgbColor: colorPalette.Vibrant ? formatColor(colorPalette.Vibrant.rgb) : 'rgb(75, 75, 75)',
+      population: colorPalette.Vibrant ? colorPalette.Vibrant.population : 0,
+    },
+    darkVibrantColor: {
+      rgbColor: colorPalette.DarkVibrant ? formatColor(colorPalette.DarkVibrant.rgb) : 'rgb(220, 156, 156)',
+      population: colorPalette.DarkVibrant ? colorPalette.DarkVibrant.population : 0,
+    },
+  };
+};
+
 const createResolvers = {
   Query: {
-    async podcasts(_, { id, name, limit }) {
-      let results;
-      if (id) {
-        results = await findOnePodcast({ id });
-      } else {
-        results = await findAllPodcasts({ name, limit });
-      }
-      return results.map(podcast => ({
-        ...podcast,
-        id: podcast.collectionId,
-        name: podcast.collectionName,
-        itunesUrl: podcast.collectionViewUrl,
-      }));
+    podcasts(_, args) {
+      return resolvePodcasts(args);
     },
   },
   Podcast: {
-    episodes({ feedUrl }, { limit }) {
-      return searchEpisodes({ feedUrl, limit });
+    episodes(root, args) {
+      return resolveEpisodes(root, args);
     },
-    artworkUrls({
-      artworkUrl30: xsmall,
-      artworkUrl60: small,
-      artworkUrl100: medium,
-      artworkUrl600: large,
-    }) {
-      return { xsmall, small, medium, large };
+    artworkUrls(root) {
+      return resolveArtworkUrls(root);
     },
-    artist({
-      artistId: id,
-      artistName: name,
-      artistViewUrl: itunesUrl,
-    }) {
-      return { id, name, itunesUrl };
+    artist(root) {
+      return resolveArtist(root);
     },
-    async palette({ artworkUrl60 }) {
-      const colorPalette = await extractColors(artworkUrl60);
-      return {
-        vibrantColor: {
-          rgbColor: colorPalette.Vibrant ? formatColor(colorPalette.Vibrant.rgb) : 'rgb(75, 75, 75)',
-          population: colorPalette.Vibrant ? colorPalette.Vibrant.population : 0,
-        },
-        darkVibrantColor: {
-          rgbColor: colorPalette.DarkVibrant ? formatColor(colorPalette.DarkVibrant.rgb) : 'rgb(220, 156, 156)',
-          population: colorPalette.DarkVibrant ? colorPalette.DarkVibrant.population : 0,
-        },
-      };
+    palette(root) {
+      return resolvePalette(root);
     },
   },
 };
