49ca683e2e93ddac54d1517b0b692a38ebde9073
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-02-05 15:03:47 +0200

    :fire: Delete useless comment

--- api/db/user.ts
+++ api/db/user.ts
@@ -1,6 +1,5 @@
 import { Schema, model } from 'mongoose';
 
-// Set up a Mongoose model and pass it using module.exports
 export default model('User', new Schema({
     email: String,
     password: String,
