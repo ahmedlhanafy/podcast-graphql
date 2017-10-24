723fdc4b822c921cd83dffa3f051898c2a3f710f
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-10-24 23:16:54 +0200

    :hammer: Support nested settings object

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,5 +1,11 @@
 export default {
-  // @FIXME: this is shouldn't be pushed instead it should be added to .env
-  jwtSecret: 'itunessecret',
-  database: 'mongodb://127.0.0.1/itunes',
+  server: {
+    port: 8080,
+  },
+  database: {
+    url: 'mongodb://127.0.0.1/itunes',
+  },
+  auth: {
+    secret: 'itunessecret',
+  },
 };
